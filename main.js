a7ac20f4b86bcbc8500a670dbab108d316755d42
Su Constantino <suellen.ds.constantino@gmail.com>
2022-06-23 09:42:31 -0300

    ok

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-const criarTarefa = (evento) => {
+import BotaoConclui from "./componentes/concluiTarefas.js"
+import BotaoDeleta from "./componentes/deletaTarefa.js"
+
+     const criarTarefa = (evento) => {
 
     evento.preventDefault()
 
@@ -13,6 +16,7 @@ const criarTarefa = (evento) => {
     tarefa.innerHTML = conteudo
 
     tarefa.appendChild(BotaoConclui())
+    tarefa.appendChild(BotaoDeleta())
     lista.appendChild(tarefa)
     input.value = ""
 }
@@ -21,19 +25,6 @@ const novaTarefa = document.querySelector('[data-form-button]')
 
 novaTarefa.addEventListener('click', criarTarefa)
 
-const BotaoConclui = () => {
-    const botaoConclui = document.createElement('button')
 
-    botaoConclui.addEventListener('click', concluirTarefa)
 
-    return botaoConclui
-}
 
-const concluirTarefa = (evento)=> {
-
-const botaoConclui = evento.target 
-
-const tarefaCompleta = botaoConclui.parentElement
-
-tarefaCompleta.classList.toggle('done')
-}
